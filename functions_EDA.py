fee2e6f1242f31bea80c9ce0ef6f26b7a44c51b0
PilarDenia <pilar.denia@gmail.com>
2020-12-11 12:22:21 +0100

    first_load_of _files

---
+++ functions_EDA.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd 
+
+
+#Function explores number of Nan-Null values ​​in each Dataframe column
+def number_nan_columns(df):
+    print(('number null/Nan_values of columns Dataframe:\n').upper())
+    for i in range(len(df.columns)):
+        print(i,df.columns[i],':',df.iloc[:,i][df.iloc[:,i].isnull()].shape[0])
+
+
+#Function explores percentage of Nan-Null values ​​in each Dataframe column
+def percentage_nan_columns(df):
+    lenght_col = df.shape[0]
+    print(('Percentage nan_values of columns Dataframe:\n').upper())
+    for i in range(len(df.columns)):
+        num_nan_col = df.iloc[:,i][df.iloc[:,i].isnull()].shape[0]
+        print(str(i)+'.-',df.columns[i],':',round((num_nan_col/lenght_col)*100,4),'%\n')
+
+
+
+#Function explores percentage of Zero values in each Dataframe column
+def percentage_zeros_columns(df):
+    ar_num_zero = np.count_nonzero(df, axis=0)
+    lenght_col = df.shape[0]
+    print(('Percentage zero_values of columns Dataframe:\n').upper())
+    for i in range(len(df.columns)):
+        print(str(i)+'.-',df.columns[i],':',
+                round(((lenght_col-ar_num_zero[i])/lenght_col)*100,3),'%\n')
+        
+        
+#function drop outlayers_maximum and outlayers_minimum of DataFrame
+#return df
+
+def drop_outlayers_df(df,column):
+    #get 1est quartile and 3er quartile 
+    q1 = df[column].quantile(q=0.25)
+    q3 = df[column].quantile(q=0.75)
+    #inter quartile range
+    iqr = q3 - q1
+    
+    #get maximun
+    maximum = q3 + (1.5*iqr)
+    
+    #get minimum
+    minimum = q1 - (1.5*iqr)
+    
+    #get indexes row outlayers, max and min
+    idx_maximum = df[df[column] > maximum].index
+    idx_minimum = df[df[column] < minimum].index
+    
+    #drop rox outlayers
+    df.drop(idx_maximum, axis = 0, inplace = True)
+    df.drop(idx_minimum, axis = 0, inplace = True)
+    
+    return df
+
